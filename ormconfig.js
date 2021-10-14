1b241546f145ef0330eca2d20f3f5eab76a7515c
BrunoBudelRossi <bbrossi@inf.ufsm.br>
2021-10-14 19:56:24 -0300

    feat: add heroku configs

---
+++ ormconfig.js
@@ -0,0 +1,17 @@
+module.exports = {
+    type: 'postgres',
+    url: process.env.DATABASE_URL,
+    logging: true,
+    entities:
+        process.env.NODE_ENV === 'development'
+            ? ['src/models/*.{ts,js}']
+            : ['dist/models/*.js'],
+    migrations:
+        process.env.NODE_ENV === 'development'
+            ? ['src/database/migrations/**/*.{ts,js}']
+            : ['dist/database/migrations/**/*.js'],
+    cli: {
+        entitiesDir: 'src/models',
+        migrationsDir: ['src/database/migrations'],
+    },
+};
