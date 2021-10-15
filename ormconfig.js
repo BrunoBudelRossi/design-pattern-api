082df6d71f0866397d6ffd9cc226290db1d9a454
BrunoBudelRossi <bbrossi@inf.ufsm.br>
2021-10-14 21:29:16 -0300

    fix: change ssl config

--- ormconfig.js
+++ ormconfig.js
@@ -1,18 +1,41 @@
-module.exports = {
-    type: 'postgres',
-    url: process.env.DATABASE_URL,
-    logging: true,
-    ssl: process.env.NODE_ENV === 'development' ? false : true,
-    entities:
-        process.env.NODE_ENV === 'development'
-            ? ['src/models/*.{ts,js}']
-            : ['dist/models/*.js'],
-    migrations:
-        process.env.NODE_ENV === 'development'
-            ? ['src/database/migrations/**/*.{ts,js}']
-            : ['dist/database/migrations/**/*.js'],
-    cli: {
-        entitiesDir: 'src/models',
-        migrationsDir: ['src/database/migrations'],
-    },
-};
+if (process.env.NODE_ENV === 'development')
+    module.exports = {
+        type: 'postgres',
+        url: process.env.DATABASE_URL,
+        logging: true,
+        entities:
+            process.env.NODE_ENV === 'development'
+                ? ['src/models/*.{ts,js}']
+                : ['dist/models/*.js'],
+        migrations:
+            process.env.NODE_ENV === 'development'
+                ? ['src/database/migrations/**/*.{ts,js}']
+                : ['dist/database/migrations/**/*.js'],
+        cli: {
+            entitiesDir: 'src/models',
+            migrationsDir: ['src/database/migrations'],
+        },
+    };
+else
+    module.exports = {
+        type: 'postgres',
+        url: process.env.DATABASE_URL,
+        logging: true,
+        extra: {
+            ssl: {
+                rejectUnauthorized: false,
+            },
+        },
+        entities:
+            process.env.NODE_ENV === 'development'
+                ? ['src/models/*.{ts,js}']
+                : ['dist/models/*.js'],
+        migrations:
+            process.env.NODE_ENV === 'development'
+                ? ['src/database/migrations/**/*.{ts,js}']
+                : ['dist/database/migrations/**/*.js'],
+        cli: {
+            entitiesDir: 'src/models',
+            migrationsDir: ['src/database/migrations'],
+        },
+    };
