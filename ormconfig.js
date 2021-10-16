a44c1ebd1db269e57c6348e963b17913becc4cbf
BrunoBudelRossi <bbrossi@inf.ufsm.br>
2021-10-15 23:07:41 -0300

    fix: deploy config

--- ormconfig.js
+++ ormconfig.js
@@ -1,10 +1,13 @@
+console.log(process.env.NODE_ENV);
 if (process.env.NODE_ENV === 'development')
     module.exports = {
         type: 'postgres',
         url: process.env.DATABASE_URL,
         logging: true,
         entities: ['src/models/*.{ts,js}'],
+
         migrations: ['src/database/migrations/**/*.{ts,js}'],
+
         cli: {
             entitiesDir: 'src/models',
             migrationsDir: ['src/database/migrations'],
