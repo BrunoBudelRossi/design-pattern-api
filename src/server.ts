7622a117b51aebfa4e8cbaf745f5b382ff481833
BrunoBudelRossi <bbrossi@inf.ufsm.br>
2021-10-15 23:24:36 -0300

    fix: deploy config

--- src/server.ts
+++ src/server.ts
@@ -10,8 +10,6 @@ export const app = express();
 
 app.disable('x-powered-by');
 
-app.use(express.static(__dirname));
-
 const corsOptions: cors.CorsOptions = {
     origin: '*',
     methods: 'GET,PUT,POST,DELETE',
