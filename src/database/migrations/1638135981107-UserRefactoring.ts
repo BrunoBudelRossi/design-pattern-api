906c363cd37d7c328d45390f11fc61ceae338b26
BrunoBudelRossi <bbrossi@inf.ufsm.br>
2021-11-28 18:52:36 -0300

    fix: user refactoring migration

---
+++ src/database/migrations/1638135981107-UserRefactoring.ts
@@ -0,0 +1,17 @@
+import { MigrationInterface, QueryRunner } from 'typeorm';
+
+export class UserRefactoring1638135981107 implements MigrationInterface {
+    name = 'UserRefactoring1638135981107';
+
+    public async up(queryRunner: QueryRunner): Promise<void> {
+        await queryRunner.query(
+            `ALTER TABLE "public"."users" ADD "name" character varying NOT NULL`
+        );
+    }
+
+    public async down(queryRunner: QueryRunner): Promise<void> {
+        await queryRunner.query(
+            `ALTER TABLE "public"."users" DROP COLUMN "name"`
+        );
+    }
+}
