66727b2f6db0093c0287039d034c4e0a78d16a28
BrunoBudelRossi <bbrossi@inf.ufsm.br>
2022-02-02 22:03:35 -0300

    chore: change problem levels

--- src/models/Problem.ts
+++ src/models/Problem.ts
@@ -2,9 +2,12 @@ import { Entity, PrimaryGeneratedColumn, Column, OneToMany } from 'typeorm';
 import Answer from './Answer';
 
 export enum ProblemDifficulty {
-    BEGGINER = 'begginer',
-    MEDIUM = 'medium',
-    HARD = 'hard',
+    TRAINEE = 'Estágiario',
+    JUNIOR = 'Junior',
+    FULL = 'Pleno',
+    SENIOR = 'Sênior',
+    MASTER = 'Master',
+    SPECIALIST = 'Especialista',
 }
 
 @Entity('problems')
@@ -27,7 +30,7 @@ export default class Problem {
     @Column({
         type: 'enum',
         enum: ProblemDifficulty,
-        default: ProblemDifficulty.MEDIUM,
+        default: ProblemDifficulty.TRAINEE,
     })
     level: ProblemDifficulty;
 
