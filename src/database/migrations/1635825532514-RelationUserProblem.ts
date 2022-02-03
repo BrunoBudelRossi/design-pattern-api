b31f9684cd7bfb16e46e7d7af2396e5e1d7401c7
BrunoBudelRossi <bbrossi@inf.ufsm.br>
2022-02-02 22:20:20 -0300

    fix: problem level default value

--- src/database/migrations/1635825532514-RelationUserProblem.ts
+++ src/database/migrations/1635825532514-RelationUserProblem.ts
@@ -1,22 +1,39 @@
-import {MigrationInterface, QueryRunner} from "typeorm";
+import { MigrationInterface, QueryRunner } from 'typeorm';
 
 export class RelationUserProblem1635825532514 implements MigrationInterface {
-    name = 'RelationUserProblem1635825532514'
+    name = 'RelationUserProblem1635825532514';
 
     public async up(queryRunner: QueryRunner): Promise<void> {
-        await queryRunner.query(`CREATE TABLE "answers" ("id" uuid NOT NULL DEFAULT uuid_generate_v4(), "usedTime" integer NOT NULL, "numberTipsUsed" integer NOT NULL, "isCorrect" boolean NOT NULL, "problemId" uuid, "userId" uuid, CONSTRAINT "PK_9c32cec6c71e06da0254f2226c6" PRIMARY KEY ("id"))`);
-        await queryRunner.query(`ALTER TABLE "public"."problems" DROP CONSTRAINT "UQ_abedb6c57245ef09c052a901801"`);
-        await queryRunner.query(`ALTER TABLE "public"."problems" ALTER COLUMN "level" SET DEFAULT 'medium'`);
-        await queryRunner.query(`ALTER TABLE "answers" ADD CONSTRAINT "FK_2e92564784fe26766b01040997f" FOREIGN KEY ("problemId") REFERENCES "problems"("id") ON DELETE NO ACTION ON UPDATE NO ACTION`);
-        await queryRunner.query(`ALTER TABLE "answers" ADD CONSTRAINT "FK_1bd66b7e0599333e61d2e3e1678" FOREIGN KEY ("userId") REFERENCES "users"("id") ON DELETE NO ACTION ON UPDATE NO ACTION`);
+        await queryRunner.query(
+            `CREATE TABLE "answers" ("id" uuid NOT NULL DEFAULT uuid_generate_v4(), "usedTime" integer NOT NULL, "numberTipsUsed" integer NOT NULL, "isCorrect" boolean NOT NULL, "problemId" uuid, "userId" uuid, CONSTRAINT "PK_9c32cec6c71e06da0254f2226c6" PRIMARY KEY ("id"))`
+        );
+        await queryRunner.query(
+            `ALTER TABLE "public"."problems" DROP CONSTRAINT "UQ_abedb6c57245ef09c052a901801"`
+        );
+        await queryRunner.query(
+            `ALTER TABLE "public"."problems" ALTER COLUMN "level" SET DEFAULT 'Estágiario'`
+        );
+        await queryRunner.query(
+            `ALTER TABLE "answers" ADD CONSTRAINT "FK_2e92564784fe26766b01040997f" FOREIGN KEY ("problemId") REFERENCES "problems"("id") ON DELETE NO ACTION ON UPDATE NO ACTION`
+        );
+        await queryRunner.query(
+            `ALTER TABLE "answers" ADD CONSTRAINT "FK_1bd66b7e0599333e61d2e3e1678" FOREIGN KEY ("userId") REFERENCES "users"("id") ON DELETE NO ACTION ON UPDATE NO ACTION`
+        );
     }
 
     public async down(queryRunner: QueryRunner): Promise<void> {
-        await queryRunner.query(`ALTER TABLE "answers" DROP CONSTRAINT "FK_1bd66b7e0599333e61d2e3e1678"`);
-        await queryRunner.query(`ALTER TABLE "answers" DROP CONSTRAINT "FK_2e92564784fe26766b01040997f"`);
-        await queryRunner.query(`ALTER TABLE "public"."problems" ALTER COLUMN "level" DROP DEFAULT`);
-        await queryRunner.query(`ALTER TABLE "public"."problems" ADD CONSTRAINT "UQ_abedb6c57245ef09c052a901801" UNIQUE ("description")`);
+        await queryRunner.query(
+            `ALTER TABLE "answers" DROP CONSTRAINT "FK_1bd66b7e0599333e61d2e3e1678"`
+        );
+        await queryRunner.query(
+            `ALTER TABLE "answers" DROP CONSTRAINT "FK_2e92564784fe26766b01040997f"`
+        );
+        await queryRunner.query(
+            `ALTER TABLE "public"."problems" ALTER COLUMN "level" DROP DEFAULT`
+        );
+        await queryRunner.query(
+            `ALTER TABLE "public"."problems" ADD CONSTRAINT "UQ_abedb6c57245ef09c052a901801" UNIQUE ("description")`
+        );
         await queryRunner.query(`DROP TABLE "answers"`);
     }
-
 }
